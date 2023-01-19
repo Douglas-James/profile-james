e37627e4e452a4bc31199aa246e32ddd7c822184
Douglas-James <ryle@drumclog.com>
2023-01-19 03:28:20 -0600

    'updateing vertextNorml'

--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -1,6 +1,10 @@
 uniform sampler2D globeTexture;
 
 varying vec2 vectorUV;
+varying vec3 vertexNormal;
 void main(){
-  gl_FragColor = texture2D(globeTexture, vectorUV);
+  float intensity = 1.03 - dot(
+    vertexNormal, vec3(0.0, 0.0, 1.0));
+  vec3 atmosphere = vec3(3, 0.6, 1.04) * pow(intensity, 1.5);
+  gl_FragColor = vec4(atmosphere + texture2D(globeTexture, vectorUV).xyz, 1.0);
 }
--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -1,6 +1,8 @@
 varying vec2 vectorUV;
+varying vec3 vertexNormal;
 
 void main(){
   vectorUV = uv;
+  vertexNormal = normal;
   gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
 }

2dd990a45681874185b9f8b3fa086e64e066f515
Douglas-James <ryle@drumclog.com>
2023-01-19 23:36:26 -0600

    'update'

--- shader/atomsphereFragment.glsl
+++ shader/atomsphereFragment.glsl
@@ -2,7 +2,7 @@ varying vec3 vertexNormal;
 
 
 void main(){
-  float intensity = pow(0.9- dot(vertexNormal, vec3(0, 0, 0.1
+  float intensity = pow(0.9- dot(vertexNormal, vec3(0, 0, 1
   )), 2.0);
-  gl_FragColor = vec4(0.5, 0.3, 0.6, 0.15) * intensity;
+  gl_FragColor = vec4(0, 0.6, 0.6, 1) * intensity;
 }

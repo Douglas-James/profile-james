d55cd8504d3df24f738d37dda35ead57e020c203
Douglas-James <ryle@drumclog.com>
2023-01-23 18:11:33 -0600

    'update'

--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -3,8 +3,8 @@ uniform sampler2D globeTexture;
 varying vec2 vectorUV;
 varying vec3 vertexNormal;
 void main(){
-  float intensity = 0.2 - dot(
-  vertexNormal, vec3(0.1, 0.1, 9));
-  vec3 atmosphere = vec3(1, 0, 0) * pow(intensity, 1.5);
+  float intensity = 0.09 - dot(
+  vertexNormal, vec3(1, 0.1, 0.1));
+  vec3 atmosphere = vec3(1, 0.4, 0) * pow(intensity, 1.1);
   gl_FragColor = vec4(atmosphere + texture2D(globeTexture, vectorUV).xyz, 1.0);
 }
